21cf3dfb425f5840fb0891d48f25fab488420d43
Jack Abernethy <jabber26@gmail.com>
2016-04-11 20:42:27 -0500

    Update for react-less boggle

--- _projects/boggle-solver.js
+++ _projects/boggle-solver.js
@@ -4,22 +4,7 @@ description: Eventually will be a fully functional game of boggle. Right now, it
 repo: https://github.com/abernet2/boggle-solver
 ---
 
-function createScript(url){
-    var imported = document.createElement("script");
-    imported.src = url;
-    imported.type = 'text/javascript';
-    document.getElementsByTagName('head')[0].appendChild(imported);
-}
-
-var links = [
-    "https://rawgit.com/abernet2/boggle-solver/master/web/build/react.js",
-    "https://rawgit.com/abernet2/boggle-solver/master/web/build/react-dom.js",
-    "https://rawgit.com/abernet2/boggle-solver/master/web/build/jquery-2.2.1.min.js",
-    'https://rawgit.com/abernet2/boggle-solver/master/web/bundle.js'
-]
-
-links.forEach(createScript);
-
-var div = document.createElement("div");
-div.id = 'example';
-document.getElementById('boggle').appendChild(div);
+<script 
+    data-main="https://raw.githubusercontent.com/abernet2/boggle-solver/master/web/scripts/app.js" 
+    src="https://raw.githubusercontent.com/abernet2/boggle-solver/master/web/scripts/require.js">
+</script>
