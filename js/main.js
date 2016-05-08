8f4b426f2737ca62cff8f99a871a33e1bd96022d
Jack Abernethy <jabber26@gmail.com>
2016-05-08 18:40:58 -0500

    Add working, but unresponsive Blog-post layout.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-(function($, window){
+function log() {
+    console.log.apply(console, arguments);
+    return arguments;
+}
+
+(function($, window, undefined){
     if(!$) {
         console.log('Jquery not loaded');
     }
@@ -7,22 +12,47 @@
         return window.scrollY;
     }
 
+    function dispatchEvent(name) {
+        var event = new Event(name);
+        window.dispatchEvent(event);
+    }
+
+    function toggleBar(evt) {
+        var $bar = $(evt.target),
+            $nav = $bar.parent();
+        $bar.toggleClass('extended');
+        $nav.toggleClass('condensed');
+    }
+
     $(document).ready(function(){
-        var $navbar = $('nav'),
+        var $topnav = $('nav.top-nav'),
+            $navbar = $('nav.bar-nav'),
             $header = $('header'),
-            scrollLine = $header.height() - 100,
+            scrollLine = $header.height(),
             scrolling = false;
 
-        $(window).on('scroll', function(){
-            if(!scrolling && scrollLine < getY()) {
-                $navbar.addClass('scrolling');
+        function customEvents(){
+            var y = getY();
+            if(!scrolling && scrollLine < y) {
+                dispatchEvent('scrollPastHeader');
                 scrolling = true;
-            } else if (scrolling && scrollLine > getY()) {
-                $navbar.removeClass('scrolling');
+            } else if (scrolling && scrollLine > y) {
+                dispatchEvent('scrollUpToHeader');
                 scrolling = false;
             }
-        })
+        }
 
+        function fixNavbar() {
+            $navbar.toggleClass('fixed');
+        }
+
+        $($navbar).on('click', toggleBar);
+        $(window).on('scroll', customEvents);
+        $(window).on('scrollPastHeader', console.log.bind(console));
+        $(window).on('scrollUpToHeader', console.log.bind(console));
+
+        $(window).on('scrollPastHeader', $navbar.addClass.bind($navbar, 'fixed'));
+        $(window).on('scrollUpToHeader', $navbar.removeClass.bind($navbar, 'fixed'));
     });
 
 })(jQuery, window);
---
+++ test.md
@@ -0,0 +1,32 @@
+---
+layout: blog_post
+title: test
+header-src: "https://static.pexels.com/photos/2855/landscape-mountains-nature-lake.jpg"
+---
+
+# Hello Everyone
+
+Here is my introductory blog post. I really like the idea of blogging about what I've been working on, at the very least to keep a record of what I've been doing. Hopefully the blog can help others learn as well!
+
+Anyway, I need to write a couple of paragraphs to test the styling on, so here it goes!
+
+```javascript
+var s = "JavaScript syntax highlighting";
+alert("Here's a code block");
+```
+Here is my introductory blog post. I really like the idea of blogging about what I've been working on, at the very least to keep a record of what I've been doing. Hopefully the blog can help others learn as well!
+
+Anyway, I need to write a couple of paragraphs to test the styling on, so here it goes!
+
+```javascript
+var s = "JavaScript syntax highlighting";
+alert("Here's a code block");
+```
+Here is my introductory blog post. I really like the idea of blogging about what I've been working on, at the very least to keep a record of what I've been doing. Hopefully the blog can help others learn as well!
+
+Anyway, I need to write a couple of paragraphs to test the styling on, so here it goes!
+
+```javascript
+var s = "JavaScript syntax highlighting";
+alert("Here's a code block");
+```
